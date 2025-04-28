46f46d4623730cf36de805daef846570e5454e83
Tiger HU <tigerlilyh777@gmail.com>
2025-04-29 01:00:58 +1200

    Add demo code for HandlerThread

--- app/src/main/java/com/example/countdowndemo/MainActivity.kt
+++ app/src/main/java/com/example/countdowndemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.countdowndemo
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
@@ -9,7 +10,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var startBackgroundTaskButton: Button
     private lateinit var handler: Handler
+    private lateinit var handlerThread: HandlerThread
     private lateinit var countdownText: TextView
     private lateinit var startButton: Button
     private var count = 5
@@ -37,17 +40,41 @@ class MainActivity : AppCompatActivity() {
 
         startButton = findViewById(R.id.startButton)
         countdownText = findViewById(R.id.countdownText)
+        startBackgroundTaskButton = findViewById(R.id.startBackgroundTaskButton)
 
         startButton.setOnClickListener {
             startButton.isEnabled = false //it's better to disable the button here, immediately after click
             count = 5
             handler.post(countdownRunnable)
         }
+
+        startBackgroundTaskButton.setOnClickListener {
+            startBackgroundLogging()
+        }
+    }
+
+    private fun startBackgroundLogging() {
+        handlerThread = HandlerThread("BackgroundThread")
+        handlerThread.start()
+
+        val backgroundHandler = Handler(handlerThread.looper)
+        backgroundHandler.post(object: Runnable {
+
+            override fun run() {
+                // 模拟后台任务
+                for (i in 1..5) {
+                    Thread.sleep(1000) // 模拟耗时操作
+                    println("Background task running in ${Thread.currentThread().name}: $i")
+                }
+            }
+        })
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         // 防止内存泄漏
         handler.removeCallbacks(countdownRunnable)
+        handlerThread.quitSafely()
     }
 }
